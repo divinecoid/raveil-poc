a82b2191ae69d46e696ecccba6c4265f2df6dcdb
divinecoid <admin@divine.co.id>
2026-07-14 21:56:27 +0700

    expense so

--- app/Models/SalesOrderItem.php
+++ app/Models/SalesOrderItem.php
@@ -22,4 +22,29 @@ class SalesOrderItem extends Model
     {
         return $this->belongsTo(Product::class);
     }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($item) {
+            $product = $item->product;
+            if ($product && floatval($product->cost_price) > 0) {
+                $costPrice = floatval($product->cost_price);
+                $totalCost = $costPrice * $item->quantity;
+                
+                // Get order number for description
+                $orderNumber = $item->salesOrder?->order_number ?? 'N/A';
+                
+                \App\Models\Expense::create([
+                    'company_id' => $item->company_id ?? $item->salesOrder?->company_id,
+                    'date' => now(),
+                    'category' => 'Harga Modal',
+                    'amount' => $totalCost,
+                    'description' => 'Harga Modal untuk ' . $product->name . ' (x' . $item->quantity . ') - Sales Order: ' . $orderNumber,
+                    'status' => 'Paid',
+                ]);
+            }
+        });
+    }
 }
